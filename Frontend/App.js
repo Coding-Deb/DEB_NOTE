43cb56d69be51f5ee0645c292ec5b2c460abd51b
Coding-Deb <debanshubrahma1234@gmail.com>
2022-12-28 19:17:27 +0530

    Login Ui Cretaed

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,13 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './Screens/LoginScreen';
+import NotesScreen from './Screens/NotesScreen';
+import NoteTakingScreen from './Screens/NoteTakingScreen';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{fontSize:18,fontWeight:'700'}}>
-        APP Here
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Login'>
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Notetake' component={NoteTakingScreen} />
+        <Stack.Screen name='AllNotes' component={NotesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- Frontend/Screens/LoginScreen.js
+++ Frontend/Screens/LoginScreen.js
@@ -1,19 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+const height = Dimensions.get('window').height
+const width = Dimensions.get('window').width
 
 export default function LoginScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+    const navigation = useNavigation()
+    return (
+        <View style={styles.container}>
+            <Text style={{ fontSize: 40, fontWeight: '700', marginTop: 25, marginBottom: 25 }}>
+                Login Here
+            </Text>
+            <View style={{ borderColor: 'black', borderWidth: 2, height: height - 200, width: width - 40, alignItems: 'center', backgroundColor: '#041E42', borderRadius: 50 }}>
+                <View style={{ top: 25 }}>
+                    <TextInput
+                        style={{ borderBottomColor: 'white', borderBottomWidth: 2, height: 60, width: width - 80, padding: 15, fontSize: 20, fontWeight: '600', marginTop: 45, marginBottom: 35,color:'white' }}
+                        placeholder='Enter Your Name'
+                        placeholderTextColor='white'
+                    />
+                    <TextInput
+                        style={{borderBottomColor: 'white', borderBottomWidth: 2, height: 60, width: width - 80, padding: 15, fontSize: 20, fontWeight: '600', marginTop: 35, marginBottom: 35,color:'white' }}
+                        placeholder='Enter Your Email'
+                        placeholderTextColor='white'
+                    />
+                    <TextInput
+                        style={{ borderBottomColor: 'white', borderBottomWidth: 2, height: 60, width: width - 80, padding: 15, fontSize: 20, fontWeight: '600', marginTop: 35, marginBottom: 35,color:'white' }}
+                        placeholder='Enter Your Password'
+                        placeholderTextColor='white'
+                    />
+                </View>
+                <TouchableOpacity style={{ width: 1 / 2 * width, height: 60, backgroundColor: 'white', top: 80, justifyContent: 'center', alignItems: 'center', borderRadius: 50 }}
+                onPress={()=>{navigation.navigate('Notetake')}}
+                >
+                    <Text style={{ color: '#041E42', fontSize: 22, fontWeight: '700' }}>
+                        LOG IN
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#0CAFFF',
+        // backgroundColor: '#99FFFF',
         alignItems: 'center',
         justifyContent: 'center',
-      },
+    },
 })
